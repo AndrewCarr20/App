8ec9f973665246fdd3dbe743a9204b9172f21c68
AndrewCarr20 <andrew.carranza70@gmail.com>
2022-05-24 04:54:07 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,15 @@ const dotenv = requrie('dotenv');
 var cors = require('cors');
 const http = require('http');
 const soketIO = require('socket.io');
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const socketServer = socketIO(httpServer, {
+
+    cors: (
+        origin: '*' // for sockets to connect
+
+
+
+    )}
